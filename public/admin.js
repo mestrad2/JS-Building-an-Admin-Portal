eb7751583527e47c63950cdb2dd040d7a357d947
Manuel Estrada <manuel.estrada16@gmail.com>
2022-08-19 10:42:48 -0400

    initial commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,38 @@
 
 // Your Code Here
+async function main() {
+    let response = await fetch('http://localhost:3001/listBooks');
+    let books = await response.json();
+    books.forEach(loadBook);
+}
+
+function loadBook(book) {
+    let bookCollection = document.querySelector('#root');
+    
+    let listItem = document.createElement('li');
+    listItem.textContent = book.title;
+
+    let amountInput = document.createElement('input');
+    amountInput.value = book.quantity;
+
+    let submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+
+    submitButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: amountInput.value
+            })
+        })
+    })
+
+    listItem.append(amountInput, submitButton);
+    bookCollection.append(listItem);
+}
+
+main();
